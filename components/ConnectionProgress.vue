dae24deb8df6bbc9c9e74c21352454470e18c8d8
dbybanez <dbybanez@gmail.com>
2020-09-01 22:39:36 +0800

    Created results page to show load test results

--- components/ConnectionProgress.vue
+++ components/ConnectionProgress.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="row justify-content-center mt-5" v-if="!loadTestStatus.idle">
-    <div class="col-9">
-      <div class="progress">
-        <div
-          class="progress-bar bg-danger"
-          role="progressbar"
-          :style="progressStatus.style"
-          aria-valuenow="30"
-          aria-valuemin="0"
-        aria-valuemax="100"></div>
+  <div class="row justify-content-center">
+    <div class="col-6">
+      <div class="row justify-content-center mt-5" v-if="!loadTestStatus.idle">
+        <div class="col-9">
+          <div class="progress">
+            <div
+              class="progress-bar bg-danger"
+              role="progressbar"
+              :style="progressStatus.style"
+              aria-valuenow="30"
+              aria-valuemin="0"
+            aria-valuemax="100"></div>
+          </div>
+          <p class="my-3 text-center font-italic">{{ progressStatus.msg }}</p>
+        </div>
       </div>
-      <p class="my-3 text-center font-italic">{{ progressStatus.msg }}</p>
     </div>
   </div>
 </template>
